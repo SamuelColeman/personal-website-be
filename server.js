b2154beba1469dd4e26aa707ab92ffc03fd3801e
Samuel Coleman <smcoleman19@gmail.com>
2020-02-05 15:26:12 -0700

    Add methods for getting all projects and schools

--- server.js
+++ server.js
@@ -8,9 +8,29 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Personal Website';
 
 app.get('/', (request, response) => {
-  response.send('Personal Website');
+  response.send('Sams Data');
 });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then((projects) => {
+      response.status(200).json(projects)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
+
+app.get('/api/v1/schools', (request, response) => {
+  database('schools').select()
+    .then(schools => {
+      response.status(200).json(schools)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
