601f64f8726d33446b6aeb238823d662b5b6c9f4
Samuel Coleman <smcoleman19@gmail.com>
2020-02-06 14:33:47 -0700

    Add installs for heroku deployment

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const cors = require('cors');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Personal Website';
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.send('Sams Data');
